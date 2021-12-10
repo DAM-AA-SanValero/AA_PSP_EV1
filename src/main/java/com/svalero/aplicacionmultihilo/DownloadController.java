22c0eb732165627b587f2542e0027ae3f72e1476
Cydogamer <cydogamer@gmail.com>
2021-12-10 16:24:52 +0100

    Descargar archivo y barra de progreso

--- src/main/java/com/svalero/aplicacionmultihilo/DownloadController.java
+++ src/main/java/com/svalero/aplicacionmultihilo/DownloadController.java
@@ -1,17 +1,26 @@
 package com.svalero.aplicacionmultihilo;
 
+import com.svalero.aplicacionmultihilo.tasks.DownloadTask;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 
+import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class DownloadController implements Initializable {
 
     private String textURL;
+    private DownloadTask downloadTask;
 
 
     public TextField txtLink;
@@ -24,11 +33,38 @@ public class DownloadController implements Initializable {
     }
 
     public void startDownload(ActionEvent event){
+        try{
+            FileChooser fileChooser = new FileChooser();
+            File file = fileChooser.showSaveDialog(txtLink.getScene().getWindow());
+            if(file==null) return;
+
+            //INICIAR TAREA
+            downloadTask = new DownloadTask(textURL, file);
+
+            downloadTask.stateProperty().addListener((observableValue, oldState, newState) -> {
+                if(newState == Worker.State.SUCCEEDED){
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setContentText("Descarga terminada");
+                    alert.show();
+                }
+            });
+
+            progressBar.progressProperty().unbind();
+            progressBar.progressProperty().bind(downloadTask.progressProperty());
+
+            downloadTask.messageProperty().addListener((observableValue, oldValue, newValue) -> labelPercent.setText(newValue));
+
+            new Thread(downloadTask).start();
+        } catch (MalformedURLException murle){
+            murle.printStackTrace();
+        }
 
     }
 
     public void stopDownload(ActionEvent event){
-
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setContentText("Descarga parada");
+        alert.show();
     }
 
     @Override
